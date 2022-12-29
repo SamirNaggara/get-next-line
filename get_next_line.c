acca81361ee9eff39badf71135ade655f0ea763b
Sam Linux <samirm.nagg@gmail.com>
2022-12-29 03:51:04 +0100

    chelou, comme d'hab

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 16:12:14 by snaggara          #+#    #+#             */
-/*   Updated: 2022/12/29 01:28:10 by snaggara         ###   ########.fr       */
+/*   Updated: 2022/12/29 03:50:14 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,80 @@
 
 char *get_next_line(int fd)
 {
-	static char buffer[BUFFER_SIZE];
 	static int	nb_line;
-	static int	cursor;
-	static int	nb_octet;
+	static struct s_static	*data_static;
 	t_data_line	*data_line;
+	char						*to_return;
 
-	if (cursor > nb_octet)
+	// Voila ce qu'il se passe QUE la première fois
+	if (!nb_line)
+	{
+		// Malloc data_static
+		data_static = (struct s_static *)malloc(sizeof(t_static));
+		if (!data_static)
+			return (NULL);
+		// Lis tout le buffer, juste la premiere fois
+		data_static->nb_octet = read(fd, data_static->buffer, BUFFER_SIZE);
+		if (data_static->nb_octet == -1)
+		{
+			free(data_static);
+			return (NULL);
+		}
+		data_static->cursor_beg = 0;
+		data_static->cursor_end = 0;
+	}
+	if (data_static->cursor_beg > data_static->nb_octet)
 		return (NULL);
 
 	// Initialise la structure data line 
 	data_line = (t_data_line *)malloc(sizeof(t_data_line));
 	if (!data_line)
-		return (NULL);
-
-
-	// Voila ce qu'il se passe QUE la première fois
-	if (!nb_line)
 	{
-		// Lis tout le buffer, juste la premiere fois
-		nb_octet = read(fd, buffer, BUFFER_SIZE);
-		if (nb_octet == -1)
-		{
-			free(data_line);
-			return (NULL);
-		}
-		cursor = 0;
+		free(data_static);
+		return (NULL);
 	}
-	// On range la valeur trouvée dans la structure, pour y avoir accès dans les fonctions
-	data_line->nb_octet = nb_octet;
-	data_line->cursor = cursor;
+
+
 	// On va chercher la longueur du buffer
-	data_line->length_line = get_length_line_buffer(data_line, buffer);
-	data_line->to_return = (char *)malloc(sizeof(char) * (data_line->length_line + 1));
-	if (!data_line->to_return)
+	data_line->length_line = get_length_line_buffer(data_static);
+
+	to_return = (char *)malloc(sizeof(char) * (data_line->length_line + 1));
+	if (!to_return)
 	{
 		free(data_line);
+		free(data_static);
 		return (NULL);
 
 	}
-	ft_strlcpy(data_line->to_return, buffer + cursor, data_line->length_line + 1);
-	cursor = data_line->cursor;
+	ft_memset(to_return, 0, data_line->length_line + 1);
+	ft_strlcpy(to_return, data_static->buffer + data_static->cursor_beg, data_line->length_line + 1);
+	//free(data_line);
+	data_static->cursor_beg = data_static->cursor_end + 1;
+
 	free(data_line);
-	cursor++;
+
 	nb_line++;
-	return (data_line->to_return);
+	return (to_return);
 }
 
-int	get_length_line_buffer(t_data_line *data_line, char *buffer)
+int	get_length_line_buffer(struct s_static *data_static)
 {
 	int		i;
+	int		cursor;
 
+	cursor = data_static->cursor_beg;
 	i = 0;
-	while (data_line->cursor < data_line->nb_octet &&
-		(buffer[data_line->cursor] != '\n'
-		&& buffer[data_line->cursor] != '\r'))
+	while (cursor < data_static->nb_octet &&
+		(data_static->buffer[cursor] != '\n'
+		&& data_static->buffer[cursor] != '\r'))
 	{
 		i++;
-		data_line->cursor = data_line->cursor + 1;
+		cursor++;
 	}
+	data_static->cursor_end = cursor;
 	return ((int)i);
 }
+
 
 
 int	main()
@@ -85,12 +98,16 @@ int	main()
 	fd = open("test.txt", O_RDONLY);
 	if (!fd)
 		return (0);
+	//printf("%s\n",get_next_line(fd));
+	t_static.cursor_beg = 8;
+	if (t_static.cursor_beg == 7)
+		return (1);
+
 	
 	while (chaine = get_next_line(fd))
 	{
 		printf("%s\n",chaine);
 		free(chaine);
-
 	}
 
 	close(fd);
--- get_next_line.h
+++ get_next_line.h
@@ -7,21 +7,30 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+
+
 typedef struct s_data_line
 {
-	int				nb_octet;
-	int				cursor;
 	int				length_line;
-	char			*to_return;
 }t_data_line;
 
+static struct s_static {
+	char	buffer[BUFFER_SIZE];
+	int		cursor_beg;
+	int		cursor_end;
+	int		nb_octet;
+}t_static = {
+  .cursor_beg = 0,  // Initialiser cursor_beg à 0
+  .cursor_end = 0,  // Initialiser cursor_end à 0
+  .nb_octet = 0  // Initialiser nb_octet à 0
+};
 char 	*get_next_line(int fd);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *str);
-int	get_length_line_buffer(t_data_line *data_line, char *buffer);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+int	get_length_line_buffer(struct s_static *data_static);
 
 #endif
 
